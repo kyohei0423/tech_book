54a1653445d547a215c2abaef83578f54c05de39
Seo Kyohei <Kyohei@Seo.local>
2016-08-17 12:42:29 +0900

    Set routing for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'top#index'
   resources :books
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
