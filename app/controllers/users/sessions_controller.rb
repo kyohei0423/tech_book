43b3697794586ba222141ddee34710fc3acbe011
Seo Kyohei <Kyohei@Seo.local>
2016-08-17 21:57:41 +0900

    Add sign in function

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
+  def create
+    super
+  end
+
   def destroy
     super
+  end
+
+  def after_sign_in_path_for(resource)
+    books_path
   end
 
   def after_sign_out_path_for(resource)
