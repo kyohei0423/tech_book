827093a28bf68dcf55cb4e7d1e21704220c351f3
Seo Kyohei <Kyohei@Seo.local>
2016-08-17 22:14:08 +0900

    Set redirect to top page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,11 @@
 class BooksController < ApplicationController
+  before_action :check_user_signed_in
+
   def index
+  end
+
+  private
+  def check_user_signed_in
+    redirect_to root_path unless current_user
   end
 end
