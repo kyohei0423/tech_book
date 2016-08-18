24787961af6018af4c56c3296d752164a09feeae
Seo Kyohei <Kyohei@Seo.local>
2016-08-18 14:07:57 +0900

    Add record to images table

---
+++ db/fixtures/02_image.rb
@@ -0,0 +1,13 @@
+Image.seed(:id,
+  { id: 1, content: "https://images-fe.ssl-images-amazon.com/images/I/515jBUQhvcL._SY346_.jpg", book_id: 1 },
+  { id: 2, content: "https://images-fe.ssl-images-amazon.com/images/I/51YrQYJdmPL._AC_US320_FMwebp_QL65_.jpg", book_id: 2 },
+  { id: 3, content: "https://images-fe.ssl-images-amazon.com/images/I/51SnYY0PNTL._AC_US320_QL65_.jpg", book_id: 3 },
+  { id: 4, content: "https://images-fe.ssl-images-amazon.com/images/I/51TXQKJm-GL._AC_US320_QL65_.jpg", book_id: 4 },
+  { id: 5, content: "https://images-fe.ssl-images-amazon.com/images/I/51AM4y1m41L._AC_US320_QL65_.jpg", book_id: 5 },
+  { id: 6, content: "https://images-fe.ssl-images-amazon.com/images/I/51RPT4ckkGL._AC_US320_QL65_.jpg", book_id: 6 },
+  { id: 7, content: "https://images-fe.ssl-images-amazon.com/images/I/41aOFsD0MDL._AC_US320_QL65_.jpg", book_id: 7 },
+  { id: 8, content: "https://images-fe.ssl-images-amazon.com/images/I/512wo9i9iSL._AC_US320_QL65_.jpg", book_id: 8 },
+  { id: 9, content: "https://images-fe.ssl-images-amazon.com/images/I/51-exJvkvqL._AC_US320_QL65_.jpg", book_id: 9 },
+  { id: 10, content: "https://images-fe.ssl-images-amazon.com/images/I/51nSEOKWSrL._AC_US320_QL65_.jpg", book_id: 10 },
+  { id: 11, content: "https://images-fe.ssl-images-amazon.com/images/I/51-S79M0yJL._AC_US320_QL65_.jpg", book_id: 11 }
+)
