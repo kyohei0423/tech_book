fc2ac917f6163f6e273cf75a901b0d868699dc08
Seo Kyohei <Kyohei@Seo.local>
2016-08-18 15:16:03 +0900

    Fix view, books/show
    - show data in table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,11 @@ class BooksController < ApplicationController
     @books = Book.order('created_at desc').includes(:image)
   end
 
+  def show
+    @book = Book.find(params[:id])
+    @related_books = Book.includes(:image).take(3)
+  end
+
   private
   def check_user_signed_in
     redirect_to root_path unless current_user
